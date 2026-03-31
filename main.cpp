cc4baf5a71dd28b022404a05be83e0e5c02f2524
amjad <amjadalariqi06@gmail.com>
2026-04-01 00:51:51 +0300

    building delete system

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
+#include <cstdio>
 
 using namespace std;
 
@@ -18,8 +20,68 @@ void addBook()
     library.close();
 }
 
+void showBooks()
+{
+    string BookName,author,avail;
+    ifstream library;
+    library.open("library.txt");
+    while(getline(library,BookName,',') && getline(library,author,',') && getline(library,avail))
+    {
+        cout<<left<<"Book name : "<<setw(20)<<BookName<<"Author : "<<setw(12)<<author;
+        if (avail == "1")
+        {
+            cout<<"Availability : Yes"<<endl;
+        }
+        else
+        {
+            cout<<"Availability : No"<<endl;
+        }
+    }
+}
+void deletebook()
+{
+    string bookname,author,avail,target;
+    bool found = false;
+
+    cout<<"Enter the book name : ";
+    //cin.ignore();
+    getline(cin,target);
+
+    ofstream tempo;
+    tempo.open("temp.txt");
+    ifstream library("library.txt");
+    if(tempo.is_open() && library.is_open()){
+    while(getline(library,bookname,',') && getline(library,author,',') && getline(library,avail))
+    {
+        if (bookname == target)
+        {
+            found = true;
+        }
+        else
+        {
+            tempo<<bookname<<','<<author<<','<<1<<endl;
+        }}}
+            library.close();
+            tempo.close();
+
+        if (found){
+            cout<<"deleted successfully!\n";
+
+            remove("library.txt");
+            rename("temp.txt","library.txt");}
+
+        else
+        {
+            cout<<"not found\n";
+            remove("temp.txt");
+        }
+
+
+
+}
 int main()
 {
-    addBook();
-    return 0;
+    //addBook();
+    deletebook();
+    //return 0;
 }
